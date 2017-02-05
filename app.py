5d504fa11aa89cb16c58688194a2d74a228b689e
Kunle Adeyinka <aadeyink@gmail.com>
2017-02-05 06:41:42 +0000

    added text processing with requests, bs4 and nltk

--- app.py
+++ app.py
@@ -1,18 +1,59 @@
 import os
-from flask import Flask
+import requests
+import operator
+import re
+import nltk
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
+from stop_words import stops
+from collections import Counter
+from bs4 import BeautifulSoup
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import Result
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
+    errors = []
+    results = {}
+    
+    if request.method == "POST":
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+        except:
+            errors.append("Unable to get URL. Please make sure it's valid and try again.")
+            return render_template('index.html', errors)
+            
+        if r:
+            # text processing
+            raw = BeautifulSoup(r.text, 'html.parser').get_text()
+            nltk_path = '{}/nltk_data/'.format(APP_ROOT)
+            nltk.data.path.append(nltk_path)  # set the path
+            tokens = nltk.word_tokenize(raw)
+            text = nltk.Text(tokens)
+            # remove punctuation, count raw words
+            nonPunct = re.compile('.*[A-Za-z].*')
+            raw_words = [w for w in text if nonPunct.match(w)]
+            raw_word_count = Counter(raw_words)
+            # stop words
+            no_stop_words = [w for w in raw_words if w.lower() not in stops]
+            no_stop_words_count = Counter(no_stop_words)
+            # save the results
+            results = sorted(no_stop_words_count.items(),key=operator.itemgetter(1),reverse=True)
+            try:
+                result = Result(url=url,result_all=raw_word_count,result_no_stop_words=no_stop_words_count)
+                db.session.add(result)
+                db.session.commit()
+            except:
+                errors.append("Unable to add item to database.")
+    return render_template('index.html',errors=errors, results=results)
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)), debug=True)
